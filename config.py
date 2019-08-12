ef06447255e73016057285e35a0ed3b03bac2b03
Martin R. Albrecht <martinralbrecht@googlemail.com>
2019-08-12 11:13:41 +0100

    whitespace

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ class MagicConstants:
 
     t_div_toffoli = 7
     t_depth_div_toffoli = 3
-    gates_div_toffoli = 17 # 7 T, 7 CNOT, 2 H, 1 S
+    gates_div_toffoli = 17  # 7 T, 7 CNOT, 2 H, 1 S
 
     AMMR12_tof_t_count = 7
     AMMR12_tof_t_depth = 3
@@ -34,17 +34,11 @@ class MagicConstants:
     ip_div_pc = 100
 
     """
-    In a filtered quantum search we nest 'Grover search with an unknown
-    number of marked elements' inside of 'amplitude amplification with
-    an unknown success probability'. We numerically optimise the trade-off
-    between the amount of amplification (number of queries) and success
-    probability (expected number of repetitions).
-    q(x) = 3*asin(sqrt(x))/(Pi + asin(sqrt(x))
-    search_amplification_factor = 1/sqrt(x)
-    search_repetition_factor = 1/(1-q(x))
+    In a filtered quantum search we nest 'Grover search with an unknown number of marked elements'
+    inside of 'amplitude amplification with an unknown success probability'.  We numerically
+    optimise the trade-off between the amount of amplification (number of queries) and success
+    probability (expected number of repetitions).  q(x) = 3*asin(sqrt(x))/(Pi + asin(sqrt(x))
+    search_amplification_factor = 1/sqrt(x) search_repetition_factor = 1/(1-q(x))
     """
-    #filter_amplification_factor = 1.70130 # x = 0.3454915028
-    #filter_repetition_factor    = 2.00000 # x = 0.3454915028
-    filter_amplification_factor = 1.67199 # x = 0.3577070585
-    filter_repetition_factor    = 2.03439 # x = 0.3577070585
-
+    filter_amplification_factor = 1.67199  # x = 0.3577070585
+    filter_repetition_factor    = 2.03439  # x = 0.3577070585
